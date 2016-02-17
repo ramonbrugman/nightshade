c275f4c29beda6ecb7f0281d8c4e3bf532b83cac
Joseph Cortbawi <josephcortbawi@gmail.com>
2016-02-16 21:28:51 -0500

    Add documentation to feature detection blocks

--- app/assets/js/pages/BasePage.js
+++ app/assets/js/pages/BasePage.js
@@ -2,6 +2,7 @@
  * @overview Base module to be imported as the foundation for all pages
  * @module BasePage.js
  * @todo Move `touch` class injection into function if we keep Feature.js
+ * @todo Modularize the init() method
 */
 
 import feature from 'viljamis/feature.js';
@@ -9,24 +10,32 @@ import attachFastClick from 'fastclick';
 
 export const BasePage = {
 
-init() {
-  console.log(` base page activated `);
+  init() {
+    console.log(` base page activated `);
 
-  if (`touchAction` in document.body.style) {
-    document.body.classList.add(`no-touch-delay`);
-  } else {
-    attachFastClick(document.body);
-  }
+    /**
+     * Checks for browser support of the `touch-action` CSS property and if so,
+     * adds a class to the body to prevent the 300ms delay on touch devices.
+     * If the property isn't supported, the FastClick polyfill is instantiated.
+    */
+    if (`touchAction` in document.body.style) {
+      document.body.classList.add(`no-touch-delay`);
+    } else {
+      attachFastClick(document.body);
+    }
 
-  if (feature.touch) {
-    document.documentElement.classList.add(`touch`);
-  } else {
-    document.documentElement.classList.add(`no-touch`);
-  }
+    /**
+     * Checks if the user is on a touch device and if so, adds the `touch` class
+     * to the HTML element. If not, the `no-touch` class is added.
+    */
+    if (feature.touch) {
+      document.documentElement.classList.add(`touch`);
+    } else {
+      document.documentElement.classList.add(`no-touch`);
+    }
 
-//  Configure nunjucks
-const env = nunjucks.configure([`app/views/`, `node_modules/@casper/`], { autoescape: true, watch: false });
+  //  Configure nunjucks
+  const env = nunjucks.configure([`app/views/`, `node_modules/@casper/`], { autoescape: true, watch: false });
 
-},
-
+  },
 };

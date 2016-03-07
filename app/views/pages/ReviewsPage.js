6a0b4a3d5c9fe9daffb2a4432b2b85bb5e2909bd
claudina sarahe <cs@misscs.me>
2016-03-07 18:54:25 -0500

    Update paths to nightshade-core

---
+++ app/views/pages/ReviewsPage.js
@@ -0,0 +1,13 @@
+import { BasePage } from 'assets/js/pages/BasePage.js';
+import { Popovers } from 'nightshade-core/src/popovers/Popovers.js';
+
+export const ReviewsPage = {
+
+  __proto__: BasePage,
+
+  init() {
+    this.__proto__.init();
+    Popovers.popover({togglerSelector: `.js-popover-toggle`, popoverSelector: `.js-popover`});
+  },
+
+};

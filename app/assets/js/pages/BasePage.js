dfd6843e238feaeae944d9186c2ce683ae26dd51
Joseph Cortbawi <josephcortbawi@gmail.com>
2016-03-07 18:50:41 -0500

    Add hamburger, make drawer work

--- app/assets/js/pages/BasePage.js
+++ app/assets/js/pages/BasePage.js
@@ -7,6 +7,7 @@
 
 import feature from 'viljamis/feature.js';
 import attachFastClick from 'fastclick';
+import pepjs from 'pepjs';
 import { ImgixSettings } from 'node_modules/@casper/nightshade-core/src/media/ImgixSettings';
 
 export const BasePage = {
@@ -14,6 +15,8 @@ export const BasePage = {
 
   init() {
     console.log(` base page activated `);
+
+    this.navDrawer = document.getElementById(`nav-main`);
 
     ImgixSettings.init();
 
@@ -38,8 +41,19 @@ export const BasePage = {
       document.documentElement.classList.add(`no-touch`);
     }
 
-  //  Configure nunjucks
-  const env = nunjucks.configure([`app/views/`, `node_modules/@casper/`], { autoescape: true, watch: false });
+    document.getElementById(`menu-trigger`).addEventListener(`pointerup`, (e) => {
+      e.stopPropagation();
+      this.navDrawer.classList.toggle(`is-invisible`);
+    });
+
+    document.addEventListener(`pointerup`, (e) => {
+      if (!e.target.classList.contains(`nav--main`, `menu-trigger`)) {
+        this.navDrawer.classList.add(`is-invisible`);
+      }
+    });
+
+    //  Configure nunjucks
+    const env = nunjucks.configure([`app/views/`, `node_modules/@casper/`], { autoescape: true, watch: false });
 
   },
 };

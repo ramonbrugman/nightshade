64cfc4d90b76f7e3a55c85b3617a9aa598e75262
Joseph Cortbawi <josephcortbawi@gmail.com>
2016-03-23 16:20:39 -0400

    Add starry background, install waypoints
    Create LandingPage.js to initialize starry background and waypoints
    Setup sticky nav using waypoints

---
+++ app/views/pages/LandingPage.js
@@ -0,0 +1,30 @@
+/**
+ * @overview Sets up the Credibility Landing page modules
+ * @module LandingPage.js
+*/
+
+import { BasePage } from 'assets/js/pages/BasePage.js';
+import { StarryBackground } from 'nightshade-core/src/backgrounds/StarryBackground.js';
+import waypoints from 'waypoints/lib/noframework.waypoints.min.js';
+
+export const LandingPage = {
+
+  __proto__: BasePage,
+
+  init() {
+    this.__proto__.init();
+    StarryBackground.init();
+    this.setWaypoints();
+  },
+
+  setWaypoints() {
+    new Waypoint({
+      element: document.querySelector(`.content-panel--engineering`),
+      handler: () => {
+        document.querySelector(`.landing-nav`).classList.toggle(`is-sticky`);
+      },
+      offset: 90,
+    });
+  },
+
+};

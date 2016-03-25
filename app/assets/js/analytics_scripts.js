8d1db1293632ec6e0658c01367c99c9e1cccdef8
Edward Friedman <edward.s.friedman@gmail.com>
2016-03-25 16:54:52 -0400

    Add JSDoc style comments

--- app/assets/js/analytics_scripts.js
+++ app/assets/js/analytics_scripts.js
@@ -1,10 +1,21 @@
-// segment
+/**
+ * Analytics Scripts
+ * @module analytics_scripts.js
+ * @overview Loads tracking service snippets: Optimizely, Segment, and Convertro
+ */
+
+/** 
+ * Segment.io snippet. Loaded asynchronously. Not deferred. 
+ * @todo: Implement env check. Swap to key "470f745508" for production.
+ */
 !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.1.0";
-analytics.load("qtm71bghig"); // Production key: "470f745508"
+analytics.load("qtm71bghig"); 
 analytics.page()
 }}();
 
-// convertro
+/** 
+ * Convertro snippet. Loaded asynchronously. Not deferred. 
+ */
 (function(c){var e=document.createElement("script");
 e.type='text/javascript';e.async=true;
 e.src='//d1ivexoxmp59q7.cloudfront.net/'+c+'/live.js';

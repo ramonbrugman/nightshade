ea0776e3e75fedd36135aeec2de8fbca7a8a4f93
Joseph Cortbawi <josephcortbawi@gmail.com>
2016-04-06 11:47:16 -0400

    Make static page JS load, add tooltips/popovers JS

--- app/views/pages/ReviewsPage.js
+++ app/views/pages/ReviewsPage.js
@@ -1,5 +1,6 @@
 import { BasePage } from 'assets/js/pages/BasePage.js';
 import { Popovers } from 'nightshade-core/src/popovers/Popovers.js';
+import { Tooltips } from 'nightshade-core/src/tooltips/Tooltips.js';
 
 export const ReviewsPage = {
 
@@ -8,6 +9,7 @@ export const ReviewsPage = {
   init() {
     this.__proto__.init();
     Popovers.popover({togglerSelector: `.js-popover-toggle`, popoverSelector: `.js-popover`});
+    Tooltips.tooltip({togglerSelector: `.js-tooltip-toggle`, tooltipSelector: `.js-tooltip`});
   },
 
 };

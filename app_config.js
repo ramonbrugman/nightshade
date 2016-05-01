2fffdf9611a43877c09d909e0eaee915483a1fd9
claudina sarahe <cs@misscs.me>
2016-05-01 17:23:57 -0400

    Add app_config.js
    File for public paths and settings

---
+++ app_config.js
@@ -0,0 +1,15 @@
+export const config = {
+  "staging": {
+    "s3": {
+      "region": "website-us-east-1",
+      "bucket": "staging-nightshade.rocks"
+    }
+  },
+  "production": {
+    "s3": {
+      "region": "website-us-east-1",
+      "bucket": "nightshade.rocks"
+    }
+  }
+};
+

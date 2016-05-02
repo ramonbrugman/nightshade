83b856a20c7c7b0d5358d4dccb217a39aa6518b0
claudina sarahe <cs@misscs.me>
2016-05-01 23:55:54 -0400

    Config paths and directories for gulp tasks

--- app_config.js
+++ app_config.js
@@ -1,15 +1,51 @@
-export const config = {
-  "staging": {
-    "s3": {
-      "region": "website-us-east-1",
-      "bucket": "staging-nightshade.rocks"
-    }
-  },
-  "production": {
-    "s3": {
-      "region": "website-us-east-1",
-      "bucket": "nightshade.rocks"
-    }
-  }
-};
+/**
+ * @file Configuration for application
+ * @module config
+ * @todo Move to nightshade-core
+ */
 
+
+/* Convinence object for long directory names  */
+const dir = {
+  nightshade: `./node_modules/@casper/nightshade-core/src`
+}
+
+export const config = {
+  paths: {
+    nightshade: `${dir.nightshade}`,
+    manifest: `./manifests`,
+    tmp: {
+      base: `./.tmp`,
+      styles: './.tmp/assets/css',
+      images: `./.tmp/assets/img`,
+      scripts: `./.tmp/assets/js`,
+      views: `./.tmp/views/`,
+    },
+    src: {
+      styles: `app/assets/scss/**/*.scss`,
+      views: `./app/views/**/[^_]*.html`,
+      tpls: [
+        `./app/views/**/_*.html`,
+        `${dir.nightshade}/**/*.html`
+      ],
+      imagesOptim: `./app/assets/img/**/*.{svg,png,gif}`,
+      images: `./app/assets/img/**/!(*.svg|*.png|*.gif)`,
+    },
+    build: {
+      base: `./dist/**/*`,
+      styles: `./dist/static-assets/css`,
+      images: `./dist/static-assets/img`,
+      scripts: `./dist/static-assets/js`,
+    },
+  },
+  s3: {
+    staging: {
+      region: `website-us-east-1`,
+      bucket: `staging-nightshade.rocks`,
+    },
+    production: {
+      region: `website-us-east-1`,
+      bucket: `nightshade.rocks`,
+    },
+  },
+};

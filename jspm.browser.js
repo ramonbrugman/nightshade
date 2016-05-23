dae071255e0e53ceb50daf8ea62510b385adb545
claudina sarahe <cs@misscs.me>
2016-05-23 03:32:05 -0400

    Move path config out of jspm.browser

--- jspm.browser.js
+++ jspm.browser.js
@@ -1,10 +1,8 @@
 SystemJS.config({
   baseURL: "/",
+  production: false,
   paths: {
     "github:*": "jspm_packages/github/*",
-    "npm:*": "jspm_packages/npm/*",
-    "nightshade-core/": "node_modules/@casper/nightshade-core/src/",
-    "assets/": "app/assets/js/",
-    "views/": "app/views/"
+    "npm:*": "jspm_packages/npm/*"
   }
 });

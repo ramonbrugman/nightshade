931d19c02ef4dcea428519327b5b7f0b3f2c1ed0
claudina sarahe <cs@misscs.me>
2016-05-23 03:34:59 -0400

    Add App.js
    This is the main js file used to build production bundles

---
+++ app/assets/js/App.js
@@ -0,0 +1,10 @@
+import { Core } from 'assets/Core.js';
+import { NavDrawer } from 'views/layout/NavDrawer.js';
+import { PatternPage } from 'views/layout/PatternPage.js';
+import { TooltipsPage } from 'views/tooltips/TooltipsPage.js';
+import { PopOversPage } from 'views/popovers/PopOversPage.js';
+import { IconsPage } from 'views/icons/IconsPage.js';
+
+Core.init();
+NavDrawer.init();
+PatternPage.init();
